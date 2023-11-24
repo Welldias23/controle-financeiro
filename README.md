2d23c411bfb0c3371a32bd094f1184ef5311e0ac
Wellington Ribeiro Dias <wellingtond712@gmail.com>
2023-11-23 21:05:53 -0300

    Arquivos adicionados ao commit anterior.

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
 # API controle financeiro
-
-- Usuario
-  - CRUD de usuario
-    - CRUD de cartão
-    - CRUD de gastos
-    - CRUD de receitas
-
-## O que vai fazer
-
-- Vai calcular as despesas e as receitas
-  - Calcular quanto estou gastando com cada tipo
-- Vai me informar quanto eu posso gastar no mes
-- Vai me enviar alertas
-  - De vencimento de fatura
-  - De limite de gasto no mes
--- dump.sql
+++ dump.sql
@@ -5,5 +5,25 @@ create table users (
   name varchar(20) not null,
   lastname varchar(20),
   email varchar(30) not null,
-  password varchar(6) not null
+  password varchar not null
+);
+
+
+create table cards_credit_debit (
+  id serial primary key,
+  id_user integer references users(id) not null,
+  invoice_due_date date,
+  card_limit int,
+  name_card varchar(10) not null
+);
+
+create table card_shopping (
+  id serial primary key,
+  id_card integer references cards_credit_debit(id) not null,
+  credit boolean not null,
+  value_total int not null,
+  installments int,
+  installment_value int,
+  purchase_date timestamp not null,
+  Type varchar(20) not null
 );
